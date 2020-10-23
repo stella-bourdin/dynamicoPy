e1dc73945125d8948873dd12dab89f92a46ac392
Stella <stella.bourdin@student.ecp.fr>
2020-10-23 11:40:08 +0200

    Add a file for basins definitions

---
+++ dynamicopy/basins.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Oceanic basins are defined with tables [[lon_min, lon_max], [lat_min, lat_max]]
+# Basins such as defined by Camargo & Zebiak, 2002.
+
+basins_CZ = {"NI": [[45, 100], [0, 40]],      # North Indian
+             "SI": [[30, 105], [-40, 0]],     # South Indian
+             "AUS": [[105, 165], [-40, 0]],   # Australia
+             "WNP": [[100, 200], [0, 40]],    # West North Pacific
+             "SP": [[165, 240], [-40, 0]],    # South Pacific
+             "ENP": [[200, 290], [0, 40]],    # East North Pacific
+             "ATL": [[260, 340], [0, 40]]}    # Atlantic
