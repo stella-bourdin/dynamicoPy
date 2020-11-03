ef9dcf5bb8f2c08f6c7ce847d25b7183b97d5f4e
Stella <stella.bourdin@student.ecp.fr>
2020-11-03 11:57:55 +0100

    Update classifiers

--- setup.py
+++ setup.py
@@ -15,11 +15,16 @@ setuptools.setup(
     # url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GPLv3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
     install_requires=["numpy", "matplotlib", "netCDF4"],
     include_package_data=True,
 )
+
+# To generate the distribution :
+# 1 / Check that wheel is up to date with `pip install --user --upgrade setuptools wheel`
+# 2 / Run `python setup.py sdist bdist_wheel`
