4c46993baefd317c4535eb31ef0f60af005eceee
Stella <stella.bourdin@student.ecp.fr>
2020-11-03 15:50:03 +0100

    Updates package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dynamicopy",
-    version="0.1",
+    version="0.2.1",
     author="Stella Bourdin",
     author_email="stella.bourdin@lsce.ipsl.fr",
     description="A set of tool to use and analyse netCDF data",
@@ -25,6 +25,12 @@ setuptools.setup(
     include_package_data=True,
 )
 
-# To generate the distribution :
+# To generate the distribution:
 # 1 / Check that wheel is up to date with `pip install --user --upgrade setuptools wheel`
 # 2 / Run `python setup.py sdist bdist_wheel`
+
+# To upload to PyPI:
+# 1 / Check that twine is up-to-date `pip install --user --upgrade twine`
+# 2 / Upload with `python -m twine upload --repository pypi dist/*`
+# 
+
