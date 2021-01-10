66c850d4af6ecb4843986bd76c2b8b1ca2aef715
sbourdin <sbourdin@LSCE.IPSL.FR>
2021-01-10 21:54:12 +0100

    Update imports

--- dynamicopy/__init__.py
+++ dynamicopy/__init__.py
@@ -2,7 +2,7 @@
 # If any private function or specific import is defined in a module, remove '*' and list the functions.
 from .utils import idx_closest, sign_change_detect
 from .utils_geo import *
-from .ncload import var_load, get_lon_lat
+from .ncload import *
 from .compute import *
 from .plot import lon_lat_plot, zonal_plot
 from .basins import *
