a7ae5b64883c24f8057148a217c469ba06c2746d
sbourdin <sbourdin@LSCE.IPSL.FR>
2021-09-13 13:38:19 +0200

    Option for plot_tracks with no color

--- dynamicopy/tc_maps.py
+++ dynamicopy/tc_maps.py
@@ -9,10 +9,12 @@ import cartopy.crs as ccrs
 import cartopy
 import seaborn as sns
 import numpy as np
+from matplotlib.colors import BoundaryNorm
 
+#TODO : Ajouter les lignes ?
 def plot_tracks(
     tracks,
-    intensity_col="wind10",
+    intensity_col=None,
     increasing_intensity=True,
     projection=ccrs.PlateCarree(central_longitude=180.0),
     fig_ax=None,
@@ -20,20 +22,22 @@ def plot_tracks(
     cmap="autumn_r",
 ):
     """
-    Function to plot tracks as issued from the cyclones module
+    Plot tracks from a dataset onto the globe.
+    The dataset is assumed loaded with functions of the cyclones module, thus with tis format.
 
     Parameters
     ----------
-    cmap
-    tracks: pandas DataFrame of tracks points
-    id_col: name of the column in track with cyclones id
-    id: id of the track(s) to plot
-    projection: ccrs projection to use for the plot
-    intensity_col: name of the column in tracks to use as color
+    tracks: The TC dataset in the format of the cyclones module.
+    intensity_col: Name of the intensity column to plot.
+    increasing_intensity: Is intensity increasing with increasing values of intensity_col?
+    projection: cartopy.crs projection to use for the plot.
+    fig_ax: (fig, ax) couple to use for the plot. If None, a new plot will be created.
+    figsize: Size of the new plot if fig_ax = None.
+    cmap: Name of the palette to use for intensity.
 
     Returns
     -------
-    None. Produces a plot.
+    A map of the tracks.
     """
 
     # Plotting
@@ -51,18 +55,43 @@ def plot_tracks(
     else :
         size_scale = (tracks[intensity_col] - tracks[intensity_col].max()) / (
                     tracks[intensity_col].min() - tracks[intensity_col].max())
-    g = sns.scatterplot(
-        data=tracks,
-        x="lon",
-        y="lat",
-        hue=intensity_col,
-        hue_norm=tuple(np.percentile(tracks[intensity_col], [10,90])),
-        ax=ax,
-        transform=ccrs.PlateCarree(),
-        palette=cmap,
-        size=size_scale,
-        sizes=(4, 20),
-    )
+    if intensity_col != None :
+        g = sns.scatterplot(
+            data=tracks,
+            x="lon",
+            y="lat",
+            hue=intensity_col,
+            hue_norm=tuple(np.percentile(tracks[intensity_col], [10,90])),
+            ax=ax,
+            transform=ccrs.PlateCarree(),
+            palette=cmap,
+            size=size_scale,
+            sizes=(4, 20),
+        )
+    else :
+        g = sns.scatterplot(
+            data=tracks,
+            x="lon",
+            y="lat",
+            ax=ax,
+            color="k",
+            transform=ccrs.PlateCarree(),
+        )
     h,l = g.get_legend_handles_labels()
     plt.legend(h[:-6], l[:-6])
     return fig, ax
+
+def density_map(lons, lats, hist,
+                ax=None,
+                projection = ccrs.PlateCarree(central_longitude=180.),
+                cmap = "Reds",
+                vmax=None,
+                shrink_cbar = 1):
+    #if ax == None:
+
+    ax = plt.axes(projection=projection)
+    p = ax.pcolormesh(lons, lats, hist.T, transform=ccrs.PlateCarree(), cmap=cmap, shading = "nearest",
+                      vmin=0, vmax=vmax)
+    plt.colorbar(p, shrink = shrink_cbar)
+    ax.coastlines()
+    plt.show()
