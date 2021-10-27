d3a3d42fdbc1c7d22fbb00260e66d4dd07d486fb
sbourdin <sbourdin@LSCE.IPSL.FR>
2021-10-27 11:51:50 +0200

    Move get_freq

--- dynamicopy/tc/metrics.py
+++ dynamicopy/tc/metrics.py
@@ -45,3 +45,64 @@ def tc_count(tracks):
         .rename(index={"All": "global"})
         .reindex(["global", "N", "WNP", "ENP", "NI", "NATL", "S", "SP", "SI", "SA"])
     )
+
+def get_freq(tracks):
+    storms = tracks.groupby("track_id")[["season", "hemisphere", "basin"]].agg(
+        lambda x: x.value_counts().index[0]
+    )
+    storms["sshs"] = tracks.groupby("track_id")["sshs"].max()
+
+    SH = (
+        storms[storms.hemisphere == "S"] \
+        .groupby(["season", "sshs"])[["basin"]] \
+        .count() \
+        .reset_index() \
+        .pivot_table(index=["sshs"], columns="season", fill_value=0) \
+        .melt(ignore_index=False) \
+        .iloc[:, 2:] \
+        .groupby("sshs") \
+        .mean() \
+        .assign(basin="S") \
+        .reset_index() \
+    )
+
+    NH = (
+        storms[storms.hemisphere == "N"]
+        .groupby(["season", "sshs"])[["basin"]]
+        .count()
+        .reset_index()
+        .pivot_table(index=["sshs"], columns="season", fill_value=0)
+        .melt(ignore_index=False)
+        .iloc[:, 2:]
+        .groupby("sshs")
+        .mean()
+        .assign(basin="N")
+        .reset_index()
+    )
+
+    basins = (
+        storms
+        .groupby(["season", "sshs", "basin"])
+        .count()
+        .reset_index()
+        .pivot_table(index=["basin", "sshs"], columns="season", fill_value=0)
+        .melt(ignore_index=False)
+        .iloc[:, 2:]
+        .groupby(["sshs", "basin"])
+        .mean()
+        .reset_index()
+    )
+
+    freq = (
+        SH.append(NH)
+        .append(basins)
+        .pivot_table(
+            index="basin", columns="sshs", fill_value=0.0, margins=True, aggfunc=np.sum
+        )
+        .drop("All")
+    )
+    freq.loc["global"] = freq.loc["S"] + freq.loc["N"]
+    freq.columns = freq.columns.get_level_values(1)
+    return freq.reindex(
+        ["global", "N", "WNP", "ENP", "NI", "NATL", "S", "SP", "SI", "SA"]
+    )
