be8d9b215ace03a7555e0d6236327ddb7bb76f61
sbourdin <sbourdin@LSCE.IPSL.FR>
2023-01-06 11:17:31 +0100

    Convert timedeltas in lifecycle stage to hours

--- dynamicopy/tc/lifecycle.py
+++ dynamicopy/tc/lifecycle.py
@@ -4,8 +4,10 @@ def identify_lifecycle(tracks, ss = None,):
     if ss == None :
         ss = storm_stats(tracks)
     t_interm = tracks.merge(ss[["track_id", "time_slp", "time_wind"]], on = "track_id")
-    tracks = tracks.assign(lag_climax_slp = (t_interm.time - t_interm.time_slp).astype('timedelta64[h]') )
-    tracks = tracks.assign(lag_climax_wind = (t_interm.time - t_interm.time_wind).astype('timedelta64[h]') )
+    tracks = tracks.assign(lag_climax_slp = t_interm.time - t_interm.time_slp)
+    tracks = tracks.assign(lag_climax_wind = t_interm.time - t_interm.time_wind)
+    tracks["lag_climax_slp"] = tracks.lag_climax_slp.astype('timedelta64[h]')
+    tracks["lag_climax_wind"] = tracks.lag_climax_wind.astype('timedelta64[h]')
     return tracks
 
 if __name__ == "__main__":
