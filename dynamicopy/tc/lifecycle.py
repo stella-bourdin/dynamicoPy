d546a1a090e6c09f4a89b2ea13d4a061872bd662
sbourdin <sbourdin@LSCE.IPSL.FR>
2023-07-27 10:11:36 +0200

    Allow for missing data in identify_lifecycle when no point is over the med

--- dynamicopy/tc/lifecycle.py
+++ dynamicopy/tc/lifecycle.py
@@ -5,7 +5,7 @@ def identify_lifecycle(tracks, ss = None,):
     if type(ss) != pd.core.frame.DataFrame : # Compute ss if not provided
         ss = storm_stats(tracks)
     # For each point indicate time of max wind and min slp
-    t_interm = tracks.merge(ss[["track_id", "time_slp", "time_wind"]], on = "track_id")
+    t_interm = tracks.merge(ss[["track_id", "time_slp", "time_wind"]], on = "track_id", how= "left")
     #
     tracks = tracks.assign(lag_climax_slp = (t_interm.time - t_interm.time_slp).astype('timedelta64[h]').values)
     tracks = tracks.assign(lag_climax_wind = (t_interm.time - t_interm.time_wind).astype('timedelta64[h]').values)
